94f46958d63015b96bcfcdd793f55692cf7337cb
Reese Levine <reeselevine@ucsc.edu>
2024-08-26 17:16:16 -0700

    fix iriw same block

--- kernels/iriw-same-block.cu
+++ kernels/iriw-same-block.cu
@@ -16,7 +16,7 @@ __global__ void litmus_test(
         uint id_0 = shuffled_workgroup * blockDim.x + threadIdx.x;
 	uint id_0_final = id_0 % total_ids;
         bool id_0_first_half = id_0 / total_ids == 0;
-        uint id_1 = shuffled_workgroup + permute_id(threadIdx.x, kernel_params->permute_thread, blockDim.x);
+        uint id_1 = shuffled_workgroup * blockDim.x + permute_id(threadIdx.x, kernel_params->permute_thread, blockDim.x);
 	uint id_1_final = id_1 % total_ids;
         bool id_1_first_half = id_1 / total_ids == 0;
 
